4348048fe4d36315824390b21d8c906a7f68f8be
Samyak Chaudhary <117174085+immsamyak@users.noreply.github.com>
2026-08-23 00:01:04 +0545

    Fix media file serving in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -38,7 +38,11 @@ if settings.DEBUG:
         path('404/', lambda request: render(request, '404.html')),
     ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 if settings.DEBUG:
     from django.urls import re_path
